edc2d1ee7ef8e5b5716934673c3846a3c40e2ced
Richard W.M. Jones <rich@annexia.org>
2008-04-02 08:05:58 +0000

    LGPL notices.

--- bitmatch.ml
+++ bitmatch.ml
@@ -1,5 +1,21 @@
 (* Bitmatch library.
- * $Id: bitmatch.ml,v 1.6 2008-04-01 19:10:45 rjones Exp $
+ * Copyright (C) 2008 Red Hat Inc., Richard W.M. Jones
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * $Id: bitmatch.ml,v 1.7 2008-04-02 08:05:58 rjones Exp $
  *)
 
 open Printf
