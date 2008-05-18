a8bf42a6bd16d6a2067f0e1475e440dbdb5bc153
Richard W.M. Jones <rich@annexia.org>
2008-05-18 20:11:19 +0000

    Seems that the ELF fields are little-endian.

--- examples/elf.ml
+++ examples/elf.ml
@@ -11,8 +11,8 @@ let () =
   bitmatch bits with
   | { 0x7f : 8; "ELF" : 24 : string;	(* ELF magic number *)
       _ : 12*8 : bitstring;		(* ELF identifier *)
-      e_type : 16;			(* object file type *)
-      e_machine : 16			(* architecture *)
+      e_type : 16 : littleendian;	(* object file type *)
+      e_machine : 16 : littleendian	(* architecture *)
     } ->
       printf "%s: ELF binary, type %d, arch %d\n" filename e_type e_machine
 
