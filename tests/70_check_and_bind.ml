c77f727a7043742154cc624dc34770a74f3d266e
Richard W.M. Jones <rich@annexia.org>
2008-07-17 11:58:14 +0000

    Improved test.

--- tests/70_check_and_bind.ml
+++ tests/70_check_and_bind.ml
@@ -9,8 +9,8 @@ let bits = (BITSTRING { 101 : 16; 202 : 16 })
 
 let () =
   bitmatch bits with
-  | { i : 16 : check (i > 100), bind (i*4);
-      j : 16 : check (j > 200) } ->
+  | { i : 16 : check (i = 101), bind (i*4);
+      j : 16 : check (j = 202) } ->
       if i <> 404 || j <> 202 then
 	failwith (sprintf "70_check_and_bind: failed: %d %d" i j)
   | { _ } ->
